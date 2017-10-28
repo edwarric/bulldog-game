7dfb2f5983099d69921fbda1a15c20f0f3897824
kkozinski8 <32336818+kkozinski8@users.noreply.github.com>
2017-10-28 23:48:58 +0100

    move bulldog to new js file

--- Server/app.js
+++ Server/app.js
@@ -1,11 +1,16 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var players = [];
 
 app.get('/', function(req, res){
+
   res.sendFile(__dirname + '/index.html');
 });
+
+app.use("/public", express.static(__dirname + '/public'))
+
 
 io.on('connection', function(socket){
   var player;
